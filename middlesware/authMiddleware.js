b5f3cbd2a991f4d87ae755d6a825da943a72fe04
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2025-10-07 15:47:29 +0100

    m

--- middlesware/authMiddleware.js
+++ middlesware/authMiddleware.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
+const protect = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -12,3 +14,5 @@ module.exports = function (req, res, next) {
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
+
+module.exports = { protect };
